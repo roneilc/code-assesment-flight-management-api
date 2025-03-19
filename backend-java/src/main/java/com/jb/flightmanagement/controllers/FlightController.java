6dcaa46b3d11a987fa7e3ee85f354b16363b4691
roneilc <roneilc@outlook.com>
2025-03-19 17:42:44 -0400

    add search, book and delete controller methods

--- backend-java/src/main/java/com/jb/flightmanagement/controllers/FlightController.java
+++ backend-java/src/main/java/com/jb/flightmanagement/controllers/FlightController.java
@@ -1,9 +1,15 @@
 package com.jb.flightmanagement.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jb.flightmanagement.models.CreateFlightRequest;
@@ -28,7 +34,9 @@ public class FlightController {
     @PostMapping("/flights")
     public CreateFlightResponse createFlight(@Validated @RequestBody CreateFlightRequest request) {
 
-        return new CreateFlightResponse(1,"JB-202","Scheduled");
+        // return new CreateFlightResponse(1,"JB-202","Scheduled");
+        return new ResponseEntity<T>(new CreateFlightResponse(1,"JB-202","Scheduled"), HttpStatus.Ok);
+
     }
 
     /**
@@ -37,10 +45,53 @@ public class FlightController {
      * @return Flight details
      * @throws Exception
      */
-    @PostMapping("/api/flights/{id}")
+    @GetMapping("/flights/{id}")
     public GetFlightResponse getFlight() {
 
         return new GetFlightResponse();
+    }
+
+    /**
+     * Method for searching flight given origin, dest, and date
+     * @param origin origin airport code
+     * @param destination destination airport code
+     * @param date Date YYY-MM-DD
+     * @return Flight details
+     * @throws Exception
+     */
+    @GetMapping
+    public GetFlightResponse searchFlight(
+        @RequestParam(required = true) String origin,
+        @RequestParam(required = true) String destination,
+        @RequestParam(required = true) String date) {
+
+        return new GetFlightResponse();
+    }
+
+    /**
+     * Method for booking flight
+     * @param id Flight Id
+     * @return Flight details
+     * @throws Exception
+     */
+    @PostMapping("/flights/{id}")
+    public GetFlightResponse bookFlight() {
+
+        return new GetFlightResponse();
+    }
+
+    /**
+     * Method for cancelling flight
+     * @param id Flight Id
+     * @return Flight details
+     * @throws Exception
+     */
+    @DeleteMapping("/{id}/bookings/{bookingId}")
+    public ResponseEntity<String> deleteBooking(
+            @PathVariable Long id,
+            @PathVariable Long bookingId) {
+
+        return ResponseEntity.ok("Booking " + bookingId + " deleted from flight " + id);
     }
     
 }

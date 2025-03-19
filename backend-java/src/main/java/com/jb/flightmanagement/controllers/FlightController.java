4ed9660ed6c40afa4530071e83efbf0e18e1f5bc
roneilc <roneilc@outlook.com>
2025-03-18 22:10:08 -0400

    add createFlight request validation

--- backend-java/src/main/java/com/jb/flightmanagement/controllers/FlightController.java
+++ backend-java/src/main/java/com/jb/flightmanagement/controllers/FlightController.java
@@ -1,10 +1,13 @@
 package com.jb.flightmanagement.controllers;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jb.flightmanagement.models.CreateFlightRequest;
 import com.jb.flightmanagement.models.CreateFlightResponse;
 
 @RestController
@@ -16,14 +19,14 @@ public class FlightController {
      * @param flightNumber Unique identifier for the flight
      * @param origin flight airport code origin
      * @param destination flight airport code destination
-     * @param departureTime flight departure in format YYY-MM-DDTHH:MM:SSZ
-     * @param arrivalTime flight departure in format YYY-MM-DDTHH:MM:SSZ
+     * @param departureTime flight departure time in format YYY-MM-DDTHH:MM:SSZ
+     * @param arrivalTime flight arrival time in format YYY-MM-DDTHH:MM:SSZ
      * @param capacity number of passengers
      * @return
      * @throws Exception
      */
     @PostMapping("/flights")
-    public CreateFlightResponse createFlight() {
+    public CreateFlightResponse createFlight(@Validated @RequestBody CreateFlightRequest request) {
 
         return new CreateFlightResponse(1,"JB-202","Scheduled");
     }

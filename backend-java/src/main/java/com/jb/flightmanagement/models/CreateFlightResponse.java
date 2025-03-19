a26309ca1badcf8a135af16f0bb69041cb903233
roneilc <roneilc@outlook.com>
2025-03-18 21:57:39 -0400

    create flight response getter and setters

--- backend-java/src/main/java/com/jb/flightmanagement/models/CreateFlightResponse.java
+++ backend-java/src/main/java/com/jb/flightmanagement/models/CreateFlightResponse.java
@@ -14,4 +14,35 @@ public class CreateFlightResponse {
     private String flightNumber;
 
     private String status;
+
+    public CreateFlightResponse(int id, String flightNumber, String status) {
+        this.id = id;
+        this.flightNumber = flightNumber;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }

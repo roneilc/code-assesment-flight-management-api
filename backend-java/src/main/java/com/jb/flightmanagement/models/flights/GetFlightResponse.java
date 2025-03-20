130a0d5ce0f197d8f40217ee6c135682d05fccde
roneilc <roneilc@outlook.com>
2025-03-20 13:21:54 -0400

    constructor for GetFlightResponse

--- backend-java/src/main/java/com/jb/flightmanagement/models/flights/GetFlightResponse.java
+++ backend-java/src/main/java/com/jb/flightmanagement/models/flights/GetFlightResponse.java
@@ -2,9 +2,13 @@ package com.jb.flightmanagement.models.flights;
 
 import java.util.Optional;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetFlightResponse {
 
     private Optional<Flight> flight;

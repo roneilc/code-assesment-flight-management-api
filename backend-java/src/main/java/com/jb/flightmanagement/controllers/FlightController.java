8806098333a586dd7a7da6652d6ff176e069e5aa
roneilc <roneilc@outlook.com>
2025-03-20 13:17:21 -0400

    add try catch and cancel booking method

--- backend-java/src/main/java/com/jb/flightmanagement/controllers/FlightController.java
+++ backend-java/src/main/java/com/jb/flightmanagement/controllers/FlightController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jb.flightmanagement.models.bookings.BookFlightRequest;
 import com.jb.flightmanagement.models.bookings.BookFlightResponse;
 import com.jb.flightmanagement.models.bookings.Booking;
+import com.jb.flightmanagement.models.bookings.CancelBookingResponse;
 import com.jb.flightmanagement.models.flights.CreateFlightRequest;
 import com.jb.flightmanagement.models.flights.CreateFlightResponse;
 import com.jb.flightmanagement.models.flights.Flight;
@@ -43,7 +45,7 @@ public class FlightController {
      * @param departureTime flight departure time in format YYY-MM-DDTHH:MM:SSZ
      * @param arrivalTime flight arrival time in format YYY-MM-DDTHH:MM:SSZ
      * @param capacity max number of passengers
-     * @return
+     * @return status
      * @throws Exception
      */
     @PostMapping("/flights")
@@ -75,24 +77,34 @@ public class FlightController {
      * @throws Exception
      */
     @GetMapping("/flights/{id}")
-    public ResponseEntity<GetFlightResponse> getFlights(@PathVariable("id") Long id) {
+    public ResponseEntity<GetFlightResponse> getFlights(@Validated @PathVariable("id") Long id) {
 
-        Optional<Flight> flight = _flightService.getFlight(id);
+        try{
+            Optional<Flight> flight = _flightService.getFlight(id);
 
-        GetFlightResponse response = new GetFlightResponse();
-
-        response.setFlight(flight);
-
-        if (flight.isPresent()) {
+            GetFlightResponse response = new GetFlightResponse();
+    
             response.setFlight(flight);
-            response.setResponseMessage("Flight Found");
-            return ResponseEntity.ok(response);
-        } else {
-            response.setResponseMessage("Not found");
-            return ResponseEntity.ok(response);
+    
+            if (flight.isPresent()) {
+                response.setFlight(flight);
+                response.setResponseMessage("Flight Found");
+                return ResponseEntity.ok(response);
+            } else {
+                response.setResponseMessage("Not found");
+                return ResponseEntity.ok(response);
+            }
+
+        }
+        catch (Exception ex) {
+            // Exception Handling
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new GetFlightResponse(null, "Flight cannot be created"));
         }
     }
 
+
+    //TODO
     /**
      * Method for searching flight given origin, dest, and date
      * @param origin origin airport code
@@ -107,7 +119,6 @@ public class FlightController {
         @RequestParam(name = "destination", required = false) String destination,
         @RequestParam(name = "date", required = false) String date) {
 
-        
         GetFlightResponse response = new GetFlightResponse();
         return ResponseEntity.ok(response);
     }
@@ -125,32 +136,52 @@ public class FlightController {
             @PathVariable("id") Long flightId,
             @RequestBody BookFlightRequest request) {
 
-         // Attempt to book flight
-        Booking booking = _bookingService.createBooking(flightId, request);
 
-        // Create response if successful
-        BookFlightResponse response = new BookFlightResponse(
-            booking.getId(),
-            booking.getStatus()
-        );
+        try {
+            // Attempt to book flight
+            Booking booking = _bookingService.createBooking(flightId, request);
 
+            // Create response if successful
+            BookFlightResponse response = new BookFlightResponse(
+                booking.getId(),
+                booking.getStatus()
+            );
+            
         return ResponseEntity.ok(response);
+            
+        } catch (Exception ex) {
+            // Exception Handling
+            BookFlightResponse response = new BookFlightResponse();
+            return ResponseEntity.ok(response);
+        }
     }
 
-    // /**
-    //  * Method for cancelling booking
-    //  * @param id Flight Id
-    //  * @return Flight details
-    //  * @throws Exception
-    //  */
-    // @DeleteMapping("/{id}/bookings/{bookingId}")
-    // public ResponseEntity<BookFlightResponse> deleteBooking(
-    //         @PathVariable Long id,
-    //         @PathVariable Long bookingId) {
+    /**
+     * Method for cancelling booking
+     *
+     * @param flightId   The ID of the flight.
+     * @param bookingId  The ID of the booking to be cancelled.
+     * @return ResponseEntity containing a message indicating success or failure.
+     */
+    @DeleteMapping("/{id}/bookings/{bookingId}")
+    public ResponseEntity<CancelBookingResponse> deleteBooking(
+            @PathVariable("id") Long flightId,
+            @PathVariable("bookingId") Long bookingId) {
+        
+        try {
+            
+            _bookingService.cancelBooking(flightId, bookingId);
 
-    //     BookFlightResponse response = new BookFlightResponse();
-    //     return ResponseEntity.ok(response);
+            CancelBookingResponse response = new CancelBookingResponse("Booking cancelled successfully.");
 
-    // }
+            return ResponseEntity.ok(response);
+
+        } catch (Exception ex) {
+            // Exception Handling
+            CancelBookingResponse response = new CancelBookingResponse("Booking cancellation unsuccessful. Booking not found");
+            return ResponseEntity.ok(response);
+        }
+    }
+    
     
 }

8aceb92149bd2438973cdffb23aa23b9a7df4c0a
roneilc <roneilc@outlook.com>
2025-03-19 20:03:41 -0400

    update Flight class to entity

--- backend-java/src/main/java/com/jb/flightmanagement/models/Flight.java
+++ backend-java/src/main/java/com/jb/flightmanagement/models/Flight.java
@@ -1,19 +1,38 @@
 package com.jb.flightmanagement.models;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "flights")
 public class Flight {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
     private String flightNumber;
 
+    @Column
     private String origin;
 
+    @Column
     private String destination;
 
+    @Column
     private String departureTime;
 
+    @Column
     private String arrivalTime;
 
+    @Column
     private int capacity;
 }
